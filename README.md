1481a077d24030a0b19ed2c42448f3f3489f250a
alex rzehak <alexander.rzehak@gmx.de>
2019-05-04 16:34:29 +0200

    Add example program from Python/Qt-tutorial.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Alexander Rzehak
 
-Work will most likely follow. This is just to initialise the repository.
+Work will most likely follow. This is just to initialize the repository.<br/>
+Since there are no teams yet, I haven't added any additional collaborators.
+---
+In the Tutorial folder you can find my adaptation of the Python/Qt-tutorial.<br/>
+The original tutorial is found [here](http://zetcode.com/gui/pyqt5/firstprograms/).
---
+++ Tutorial/center.py
@@ -0,0 +1,45 @@
+"""
+ZetCode PyQt5 tutorial
+
+This program centers a window
+on the screen.
+
+Author: Jan Bodnar
+Website: zetcode.com
+Last edited: August 2017
+"""
+
+import sys
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle('Center')
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        # print(qr)
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        # print(qr)
+        self.move(qr.topLeft())
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
